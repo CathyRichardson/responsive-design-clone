c62dbcf1a87e5ed7dd87c9ca7cdb68318b23a5bb
Cathy Richardson <cathyanne08@gmail.com>
2021-08-03 22:06:17 -0600

    added tab index and focus

--- responsive-design/src/components/NavBar.js
+++ responsive-design/src/components/NavBar.js
@@ -9,7 +9,9 @@ const NavBar = ({ links, isMenuOpen, toggleMenu }) => {
                 <ul>
                     {links.map(link => <li>{link}</li>)}
                 </ul>
-                <a onClick={toggleMenu}>Menu<img src="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-1.jpg" /></a>
+                <a tabIndex="0" onClick={toggleMenu}>Menu
+                    <img src="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-1.jpg" />
+                </a>
             </nav>
             <menu className={isMenuOpen ? 'menu-visible' : 'menu-hidden'}>
                 {links.map(link => <li >{link}</li>)}
